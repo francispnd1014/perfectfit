0d75935d15629ade9e15be0199e0cedef0292d83
Pr1m0z <acagungun@cca.edu.ph>
2024-11-14 19:11:51 +0800

    ok

--- HTML/Logout.php
+++ HTML/Logout.php
@@ -1,7 +1,27 @@
 <?php
-session_start();
-session_unset();
+// Start session if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Clear all session data
+$_SESSION = array();
+
+// Destroy session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+// Destroy the session
 session_destroy();
-header("Location: Index.php");
+
+// Define full path to Login.php
+$login_path = dirname(__FILE__) . '/Login.php';
+if (file_exists($login_path)) {
+    header("Location: Login.php");
+} else {
+    header("Location: ../HTML/Login.php");
+}
+
 exit();
 ?>
