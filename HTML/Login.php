516c3efec7368b8dc3a34807024de18d1be6e5b1
Pr1m0z <acagungun@cca.edu.ph>
2024-11-18 21:27:42 +0800

    ds

--- HTML/Login.php
+++ HTML/Login.php
@@ -252,9 +252,9 @@ $conn->close();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https:
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="../CSS/LoginSignup.css">
     <link rel="icon" type="image/x-icon" href="../IMAGES/FAV.png">
     <title>Signup</title>
