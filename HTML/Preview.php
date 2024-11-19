8e2fd877ad822efdaf61ebab77ac28662850fe4f
Pr1m0z <acagungun@cca.edu.ph>
2024-11-19 19:11:27 +0800

    ds

--- HTML/Preview.php
+++ HTML/Preview.php
@@ -265,12 +265,12 @@ $stmt->close();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../IMAGES/FAV.png">
-    <link rel="stylesheet" href="https:
-    <link rel=" stylesheet" href="font/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="font/css/all.min.css">
     <link rel="stylesheet" href="../CSS/Preview.css">
     <title><?php echo $gown_name; ?></title>
 </head>
